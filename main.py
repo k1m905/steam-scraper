c9d67304ce5a3246e9e1c58d514fb291b8749191
Taehyun Kim <taehyun1834@gmail.com>
2024-01-21 20:01:41 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, redirect
+from steam import scrape
+
+app = Flask('Game Scrapper')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/search')
+def secondehome():
+    fileName = request.args.get('fileName')
+    if fileName == 'steam':
+        games = scrape()
+        return render_template('search.html', fileName = fileName, games = games)
+    else:
+        return redirect('/')
+
+app.run('0.0.0.0')
---
+++ steam.py
@@ -0,0 +1,44 @@
+from bs4 import BeautifulSoup
+from playwright.sync_api import sync_playwright
+import time
+
+def scrape():
+    url = 'https://store.steampowered.com/search/?filter=topsellers&os=win'
+
+    gamePriceInfo = []
+
+    p = sync_playwright().start()
+    browser = p.chromium.launch()
+    page = browser.new_page()
+    page.goto(url)
+
+    for i in range(10):
+        page.keyboard.down('End')
+        time.sleep(0.5)
+
+    content = page.content()
+
+    p.stop()
+
+    soup = BeautifulSoup(content, 'html.parser')
+
+    games = soup.find_all('a', class_='search_result_row ds_collapse_flag')
+
+    for game in games:
+        title = game.find('span', class_='title').text
+        discount = game.find('div', class_='discount_pct')
+        price = game.find('div', class_='discount_prices')
+
+        if(discount != None):
+            ogprice = price.text.strip().split('C$')[1]
+            newprice = price.text.strip().split('C$')[2]
+            gamelist = {
+                'title': title,
+                'discount': discount.text.strip(),
+                'ogprice': ogprice,
+                'newprice': newprice,
+                'link': game.get('href')
+            }
+            gamePriceInfo.append(gamelist)
+    
+    return gamePriceInfo
